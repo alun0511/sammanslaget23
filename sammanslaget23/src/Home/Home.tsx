9daf29dafe89e83cfe681a637f6cf40d392d1a46
Valisaurus <valley.alosachi@hotmail.com>
2023-08-22 11:25:32 +0200

    tried to style home with styled component

---
+++ sammanslaget23/src/Home/Home.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { HomeStyled } from "./styled";
+
+const Home: React.FC = () => {
+  return (
+    <HomeStyled>
+      <h1>Hello</h1>
+    </HomeStyled>
+  );
+};
+
+export default Home;
---
+++ sammanslaget23/src/Home/styled.tsx
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const HomeStyled = styled.div`
+  background-color: black;
+  height: 100vh;
+  width: 100vw;
+`;
