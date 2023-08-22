d97c5283641fe261d19707ae730ae9a7d6799eb3
Valisaurus <valley.alosachi@hotmail.com>
2023-08-22 14:08:32 +0200

    deletet everything for a restart

--- sammanslaget23/src/Components/Nav/styled.tsx
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-
-export const NavStyled = styled.div`
-  margin-top: 0;
-  background-color: #c15353;
-  height: 20rem;
-  width: 100vw;
-`;
--- sammanslaget23/src/Prototype/styled.tsx
+++
@@ -1,7 +0,0 @@
-import styled from "styled-components";
-
-export const PrototypeStyled = styled.div`
-  background-color: black;
-  height: 100vh;
-  width: 100vw;
-`;
